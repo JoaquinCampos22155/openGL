7dc2ac001a85c484a7381f98077148b55f757634
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-10-14 11:42:43 -0600

    Clase11/10

---
+++ RendererOpenGL.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import * 
+from gl import Renderer
+from buffer import Buffer
+from shaders import *
+width = 960
+height = 540
+
+pygame.init()
+
+screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
+clock = pygame.time.Clock()
+
+rend = Renderer(screen) 
+rend.SetShaders(vertex_shader, fragment_shader)
+triangle = [-0.5, -0.5, 0,   1,0,0, 
+               0,  0.5, 0,   0,1,0,  
+             0.5, -0.5, 0,   0,0,1]
+
+rend.scene.append( Buffer(triangle) )
+
+
+isRunning = True
+
+while isRunning:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            isRunning = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                isRunning = False
+           
+    deltaTime = clock.tick(60) / 1000
+    # print(deltaTime)
+    rend.Render()
+    pygame.display.flip()	  
+
+pygame.quit()  
+  
---
+++ buffer.py
@@ -0,0 +1,47 @@
+import glm #pip install PyGLM
+from numpy import array, float32
+#pip install PyOpenGL
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+
+class Buffer(object):
+    def __init__(self, data):
+        self.vertBuffer = array(data, float32)
+        
+        #Vertex Buffer Object
+        self.VBO = glGenBuffers(1)
+        
+        # Vertex Array Object 
+        self.VAO = glGenVertexArrays(1)
+        
+    def Render(self):
+        #atar buffer a tarjeta
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+        glBindVertexArray(self.VAO)
+        
+        # mandar info de verts
+        glBufferData(GL_ARRAY_BUFFER, #BUFFER ID 
+                     self.vertBuffer.nbytes,# buffer size in bytes
+                     self.vertBuffer, # buffer data
+                     GL_STATIC_DRAW) #usage
+        #atributo pos
+        glVertexAttribPointer(0, # attribute number
+                              3, # size
+                              GL_FLOAT, # type
+                              GL_FALSE, # is normalized
+                              4 * 6, # Stride size in bytes
+                              ctypes.c_void_p(0)) # Offset 
+        
+        glEnableVertexAttribArray(0)
+        #atributo color
+        glVertexAttribPointer(1, # attribute number
+                              3, # size
+                              GL_FLOAT, # type
+                              GL_FALSE, # is normalized
+                              4 * 6, # Stride size in bytes
+                              ctypes.c_void_p(4*3)) # Offset 
+        
+        glEnableVertexAttribArray(1)
+        
+        glDrawArrays(GL_TRIANGLES, 0, int(len(self.vertBuffer) / 6))
+        
